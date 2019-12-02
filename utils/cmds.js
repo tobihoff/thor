c086b79af5f3c4dada962d1f79a6916bdb3ac03f
tobihoff <56834053+tobihoff@users.noreply.github.com>
2019-12-02 14:54:20 +0100

    Add password.json (#3)

--- utils/cmds.js
+++ utils/cmds.js
@@ -1,18 +1,34 @@
-const passwords = {
-  wifi: 123,
-  mac: "mac321"
-};
+const fs = require("fs");
+
+function readPasswords() {
+  try {
+    const passwordJSON = fs.readFileSync(".passwords.json", "utf-8");
+    const passwords = JSON.parse(passwordJSON);
+    return passwords;
+  } catch (error) {
+    return;
+  }
+}
+
+function writePasswords(passwords) {
+  fs.writeFileSync(".passwords.json", JSON.stringify(passwords, null, 2));
+}
 
 function get(key) {
+  const passwords = readPasswords();
   return passwords[key];
 }
 
 function set(key, value) {
+  const passwords = readPasswords();
   passwords[key] = value;
+  writePasswords(passwords);
 }
 
 function unset(key) {
+  const passwords = readPasswords();
   delete passwords[key];
+  writePasswords(passwords);
 }
 
 exports.get = get;
